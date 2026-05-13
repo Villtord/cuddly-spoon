b840157d16f71510b381b60c45ea83ca33619172
p-mousley <philip.mousley@diamond.ac.uk>
2026-05-13 11:23:07 +0100

    test line

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-# test line for github account check
+# new test line for github account check
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
